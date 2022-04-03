4a22c789c732b4d5cb73da8d01564b203b294bd1
MichalScigalski <michalscigalski0@gmail.com>
2022-04-03 21:44:00 +0200

    reactDom error fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  // const [memesAll, setMemesAll] = useState[''];
+
   const fetchMeme = () => {
     return axios
-      .get("https://randomuser.me/api")
+      .get("https://api.imgflip.com/get_memes")
       .then((res) => {
-        console.log(res);
+        console.log(res.data.data.memes);
+        // console.log(memesAll);
       })
       .catch((err) => {
         console.log(err);
@@ -15,13 +19,10 @@ function App() {
   return (
     <div className="App">
       <h1>mem gen</h1>
-      <button
-        onClick={() => {
-          fetchMeme();
-        }}
-      >
-        ok
-      </button>
+      <button onClick={fetchMeme}>ok</button>
+      {/* {memesAll.map((el)=>{
+        <h1>el</h1>
+      })} */}
     </div>
   );
 }
