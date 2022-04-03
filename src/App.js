82922eb0de3913cff70e45f292633161f60db630
MichalScigalski <michalsciglaski0@gmail.com>
2022-04-03 19:04:09 +0200

    file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const fetchMeme = () => {
+    return axios
+      .get("https://randomuser.me/api")
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>mem gen</h1>
+      <button
+        onClick={() => {
+          fetchMeme;
+        }}
+      >
+        ok
+      </button>
     </div>
   );
 }
