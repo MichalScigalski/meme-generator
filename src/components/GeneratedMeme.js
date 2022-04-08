defaff47e0b20e4b762b8d8eb511e143e61359f8
MichalScigalski <michalscigalski0@gmail.com>
2022-04-08 17:29:17 +0200

    downloading meme image added

--- src/components/GeneratedMeme.js
+++ src/components/GeneratedMeme.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import './GeneratedMeme.css'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 function GeneratedMeme({ meme, onClick }) {
+    const { id } = useParams();
+    const downloadMeme = () => {
+        axios({
+            url: meme.data.url,
+            method: 'GET',
+            responseType: 'blob',
+        }).then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `${id}.jpg`);
+            document.body.appendChild(link);
+            link.click();
+        });
+    }
+
     return (
         <div className="GeneratedMeme">
+            {console.log(meme.data)}
             <button onClick={onClick}>Back</button>
             <div>
                 <img src={meme.data.url} alt="generated meme" />
             </div>
-            <a href={meme.data.url} target="_blank" download rel="noreferrer">Download</a>
+            <a onClick={downloadMeme}>Download</a>
         </div>
     )
 }
