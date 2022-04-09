e54504a2b9d366b1b87955536c6ed978e673d876
MichalScigalski <michalscigalski0@gmail.com>
2022-04-09 16:04:48 +0200

    nav appearance update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './Navigation.scss'
-import Logo from '../img/memegenicon.png';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
     return (
         <header className='Navigation'>
             <Link className='Navigation__Logo' to={"/"}>
-                <img src={Logo} alt="memeIcon" />
-                <span>Meme Generator</span>
+                <span>MemeGenerator</span>
             </Link>
         </header>
     )
