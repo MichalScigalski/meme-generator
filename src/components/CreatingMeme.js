a3ceab6d5cadfbc5dc44874aa5e21ed48048a28e
MichalScigalski <michalscigalski0@gmail.com>
2022-04-09 17:51:15 +0200

    createMeme styled

--- src/components/CreatingMeme.js
+++ src/components/CreatingMeme.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import './CreatingMeme.scss'
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import undoIcon from '../img/undo.png';
+import {
+    Link
+} from "react-router-dom";
 
 function CreatingMeme({ template, backHandler, setCreatedMeme }) {
     let navigate = useNavigate();
@@ -27,7 +31,7 @@ function CreatingMeme({ template, backHandler, setCreatedMeme }) {
             .then(res => {
                 setCreatedMeme(res.data);
                 const idMeme = res.data.data.page_url.split('/');
-                navigate("/meme/"+idMeme[idMeme.length-1]);
+                navigate("/meme/" + idMeme[idMeme.length - 1]);
             })
             .catch(err => {
                 console.log(err);
@@ -35,11 +39,14 @@ function CreatingMeme({ template, backHandler, setCreatedMeme }) {
     };
     return (
         <div className="creatingMeme">
-            <button onClick={backHandler}>Back to templates</button>
+            <a onClick={() => navigate(-1)}>
+                <img src={undoIcon} alt="undoIcon" />
+            </a>
             <form onSubmit={createMeme}>
                 <img src={template.url} title={template.name} alt={template.name + '-img'} />
-                <input onChange={e => setMemeTextFirst(e.target.value)} type="text" placeholder="first text" />
-                <input onChange={e => setMemeTextSecond(e.target.value)} type="text" placeholder="second text" />
+                <span>{template.name}</span>
+                <input onChange={e => setMemeTextFirst(e.target.value)} type="text" placeholder="First text" />
+                <input onChange={e => setMemeTextSecond(e.target.value)} type="text" placeholder="Second text" />
                 <button type="submit">Create meme</button>
             </form>
         </div>
