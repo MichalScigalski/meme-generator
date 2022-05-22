cc66f50baa0080a2c5c7c8ebdf59c1bfc36e3ecb
MichalScigalski <michalscigalski0@gmail.com>
2022-05-22 18:30:33 +0200

    navigation github link added with styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Navigation.scss'
 import { Link } from 'react-router-dom';
+import { FaGithub } from 'react-icons/fa';
 
 const Navigation = () => {
     return (
         <header className='Navigation'>
             <Link className='Navigation__Logo' to={"/"}>
                 <span>MemeGenerator</span>
+                <div>
+                    <FaGithub fontSize={22}/>
+                    <a href="http://github.com/MichalScigalski">@MichalScigalski</a>
+                </div>
             </Link>
         </header>
     )
